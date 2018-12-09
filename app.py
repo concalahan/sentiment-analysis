6e4684eefb1018b06d4b640d6e3aeb51fa26ee1c
vu <mainguyenvu210@gmail.com>
2018-12-09 23:28:31 +0700

    working on tokenizate more than 1 words

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ from textblob import TextBlob
 
 app = Flask(__name__)
 
+# global variable
+max_word_tokenize = 5
+
 @app.route('/')
 def index():
     return 'Index Page'
@@ -40,6 +43,51 @@ def is_digit(n):
         return True
     except ValueError:
         return  False
+
+def get_afinn_word_in_five_words(wordArray, afinn):
+    # working
+    if(len(wordArray) == 0):
+        print("return cmnr")
+        return None
+
+    word = ' '.join(wordArray)
+
+    print("hihihi " + str(word))
+
+    if word in afinn:
+        print("hohohoho " + str(word))
+        return word
+    else:
+        get_afinn_word_in_five_words(wordArray[:-1], afinn)
+
+        print("AAAAAAAAAAAAAAAAAAA")
+
+        get_afinn_word_in_five_words(wordArray[1:], afinn)
+
+        print("BBBBBBBBBBBBBBBBBBB")
+
+    # found = False
+
+    # # execute until found
+    # while(not found):
+    #     word = ' '.join(wordArray)
+
+    #     # check current
+    #     if word in afinn:
+    #         return word
+
+    #     word = ' '.join(wordArray[:-1])
+
+    #     # check exclude last index (rightest)
+    #     if word in afinn:
+    #         return word
+
+    #     word = ' '.join(wordArray[1:])
+
+    #     # check exclude first index (leftest)
+    #     if word in afinn:
+    #         return word
+
 
 def check(descripton):
     # positive/ negative
@@ -66,22 +114,42 @@ def check(descripton):
 
             afinn[split_line[0]] = split_line[1]
 
+    print(afinn["vô tình"])
+
     # polarity means emotions expressed in a sentence
     # how to calculate polarity? famous method is using bag of words.
     words = descripton.split()
 
     if descripton == None:
         sys.exit("Error")
+
+    temp_count = 0
+    temp_word_array = []
     for word in words:
         word = word.lower()
+        temp_word_array.append(word)
+        temp_count += 1
 
-        if word in afinn:
-            if(int(afinn[word]) > 0):
-                positive.append(word)
-            elif(int(afinn[word]) < 0):
-                negative.append(word)
+        # start perform sentiment analysis every 5 words
+        if(temp_count == max_word_tokenize):
+            print("Checking... " + str(temp_word_array))
+            
+            afinn_word = get_afinn_word_in_five_words(temp_word_array, afinn)
 
-            score += int(afinn[word])
+            print("afinn_word " + str(afinn_word))
+
+            if afinn_word is not None:
+                if(int(afinn[afinn_word]) > 0):
+                    positive.append(afinn_word)
+                elif(int(afinn[afinn_word]) < 0):
+                    negative.append(afinn_word)
+
+                score += int(afinn[afinn_word])
+
+            # set value again
+            temp_count = 0
+            temp_word_array = []
+
 
     comparative = score / len(words)
 
